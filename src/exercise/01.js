9a96a4a0fe7e7792b69a6f520b853d901efb0b7e
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-03 17:18:17 -0300

    solved context module function exercise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -8,6 +8,7 @@ import {dequal} from 'dequal'
 
 import * as userClient from '../user-client'
 import {useAuth} from '../auth-context'
+import { update } from 'lodash'
 
 const UserContext = React.createContext()
 UserContext.displayName = 'UserContext'
@@ -73,14 +74,14 @@ function useUser() {
   return context
 }
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
+function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+  userClient.updateUser(user, updates).then(
+    updatedUser => dispatch({type: 'finish update', updatedUser}),
+    error => dispatch({type: 'fail update', error}),
+  )
+}
 
-// export {UserProvider, useUser}
-
-// src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -94,15 +95,10 @@ function UserSettings() {
   function handleChange(e) {
     setFormState({...formState, [e.target.name]: e.target.value})
   }
-
+  
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState)
   }
 
   return (
